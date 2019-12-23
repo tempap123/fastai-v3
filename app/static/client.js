6230f44fb12687efdcea21b883e8740939685e8c
tempap123 <59175016+tempap123@users.noreply.github.com>
2019-12-23 22:53:06 +0530

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -16,9 +16,9 @@ function showPicked(input) {
 
 function analyze() {
   var uploadFiles = el("file-input").files;
-  if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
+  if (uploadFiles.length !== 1) alert("कृपया विश्लेषण करने के लिए एक फ़ाइल का चयन करें!");
 
-  el("analyze-button").innerHTML = "Analyzing...";
+  el("analyze-button").innerHTML = "विश्लेषण चल रहा है ...";
   var xhr = new XMLHttpRequest();
   var loc = window.location;
   xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
@@ -29,9 +29,9 @@ function analyze() {
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML = `Result = ${response["result"]}`;
+      el("result-label").innerHTML = `परिणाम = ${response["result"]}`;
     }
-    el("analyze-button").innerHTML = "Analyze";
+    el("analyze-button").innerHTML = "विश्लेषण";
   };
 
   var fileData = new FormData();
